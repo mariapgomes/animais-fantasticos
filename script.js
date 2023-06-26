e0496941e26677908b6b52ebc3d5b74abd52c080
Maria Gomes <mariapg.dev@gmail.com>
2023-06-26 15:14:44 -0300

    Cria o accordion list do faq

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ function navegacaoTab () {
 }
 
 navegacaoTab();
+
+//*Cria accordion list
+
+function iniciaAccordion () {
+  const listaFaq = document.querySelectorAll('.js-accordion dt');
+  const classeAtivo = 'ativo'
+  
+  if (listaFaq) {
+    listaFaq[0].classList.add(classeAtivo);
+    listaFaq[0].nextElementSibling.classList.add(classeAtivo);
+    
+    function ativaAccordion () {
+      this.classList.toggle(classeAtivo);
+      this.nextElementSibling.classList.toggle(classeAtivo)
+    }
+
+    listaFaq.forEach((item) => item.addEventListener('click', ativaAccordion));
+  }
+}
+
+iniciaAccordion();
