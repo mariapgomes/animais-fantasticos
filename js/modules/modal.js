3de8260b56658f9e5c3d8c1171b75ebfdd731f98
Maria Gomes <mariapg.dev@gmail.com>
2023-12-27 08:11:44 -0300

    Refatora a função de abrir e fechar o modal

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,22 +1,39 @@
-export default function iniciaModal() {
-  const btnAbrir = document.querySelector('[data-modal="abrir"]');
-  const btnFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(btnAbrir, btnFechar, containerModal) {
+    this.btnAbrir = document.querySelector(btnAbrir);
+    this.btnFechar = document.querySelector(btnFechar);
+    this.containerModal = document.querySelector(containerModal);
 
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle('ativo');
+    // Definição do this para a propriedade eventoToggleModal.
+    this.eventoToggleModal = this.eventoToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
 
-  function cliqueForaModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
+
+  eventoToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
     }
   }
 
-  if (btnAbrir && btnFechar && containerModal) {
-    btnAbrir.addEventListener('click', toggleModal);
-    btnFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
+  adicionaEvento() {
+    this.btnAbrir.addEventListener('click', this.eventoToggleModal);
+    this.btnFechar.addEventListener('click', this.eventoToggleModal);
+    this.containerModal.addEventListener('click', this.cliqueForaModal);
+  }
+
+  iniciaClasse() {
+    if (this.btnAbrir && this.btnFechar && this.containerModal) {
+      this.eventoToggleModal();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import iniciaAnimacaoScroll from './modules/scroll-animacao.js';
 import NavegacaoTab from './modules/navegacao-tab.js';
 import Accordion from './modules/accordion.js';
-import iniciaModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import iniciaTooltip from './modules/tooltip.js';
 import iniciaMenuDropdown from './modules/menu-dropdown.js';
 import iniciaMenuMobile from './modules/menu-mobile.js';
@@ -21,7 +21,9 @@ navegacaoTab.iniciaClasse();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.iniciaClasse();
 
-iniciaModal();
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.iniciaClasse();
+
 iniciaTooltip();
 iniciaMenuDropdown();
 iniciaMenuMobile();
